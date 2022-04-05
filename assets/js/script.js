1c29751549351cebaa7bb7d2671f142999ec0b13
ClaudioQD <85728597+ClaudioQD@users.noreply.github.com>
2022-04-05 15:46:43 -0400

    se implementa funcionalidades
    se implementa funcionalidades

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,17 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel)
+
+
+
+$("#titleA").click(function(){
+$("#textoA").toggle("slow");
+})
+
+$("#titleB").click(function(){
+$("#textoB").toggle("slow");
+})
+    
+$("#titleC").click(function(){
+$("#textoC").toggle("slow");
+})
